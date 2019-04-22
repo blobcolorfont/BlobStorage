c9f245559563b8ccdccbdf9efb599dcc2795ee4b
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2019-04-22 18:42:24 -0300

    Add support for W-marks, added
    This is a mess, but it works.
    People Holding Hands was the test subject, it worked but since it is too early to see if that emoji is gender-neutral or not, it's not added.
    In compensation, Man&Woman, Man&Man and Woman&Woman Couples are added.

--- build.js
+++ build.js
@@ -78,7 +78,16 @@ var skincolorvalues = ["", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"];
     haircolor = ["FFA000", "312D2D", "BFA055", "6D4C41", "47352D", "232020"],
     neckcolor = ["E59900", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
     othercolor = ["E59600", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
-    nosecolor = ["E48C15", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
+    nosecolor = ["E48C15", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"];
+
+var skincolorvaluesw = ["-test", "", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"];
+    skincolorw = ["FFFF00", "FCC21B", "FADCBC", "E0BB95", "CC9970", "AD6F43", "8E726A"],
+    handcolorw = ["FFCC00", "FAC036", "FADCBC", "E0BB95", "CC9970", "AD6F43", "8E726A"],
+    hand2colorw = ["00CCFF", "E39E49", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
+    haircolorw = ["FF0000", "FFA000", "312D2D", "BFA055", "6D4C41", "47352D", "232020"],
+    neckcolorw = ["00FF00", "E59900", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
+    othercolorw = ["0000FF", "E59600", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
+    nosecolorw = ["FF00FF", "E48C15", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
 
 console.log("Converting X-marked emojis");
 
@@ -88,22 +97,55 @@ xfiles.forEach(function (item, index) {
     let newname = item.replace(/x/g, skincolorvalues[i]);
     fs.copyFileSync(`src/all/${item}`, `svgs/${newname}`);
     var newfile = fs.readFileSync(`svgs/${newname}`, "utf8");
-    var allskin = new RegExp (skincolor[0], "gi");
-    var allhand = new RegExp (handcolor[0], "gi");
-    var allhand2 = new RegExp (hand2color[0], "gi");
-    var allhair = new RegExp (haircolor[0], "gi");
-    var allneck = new RegExp (neckcolor[0], "gi");
-    var allnose = new RegExp (nosecolor[0], "gi");
-    var allother = new RegExp (othercolor[0], "gi");
-    var newfile2 = newfile.replace(allskin, skincolor[i]);
-    var newfile2 = newfile2.replace(allhand, handcolor[i]);
-    var newfile2 = newfile2.replace(allhand2, hand2color[i]);
-    var newfile2 = newfile2.replace(allhair, haircolor[i]);
-    var newfile2 = newfile2.replace(allneck, neckcolor[i]);
-    var newfile2 = newfile2.replace(allnose, nosecolor[i]);
-    var newfile2 = newfile2.replace(allother, othercolor[i]);
+        allskin = new RegExp (skincolor[0], "gi"),
+        allhand = new RegExp (handcolor[0], "gi"),
+        allhand2 = new RegExp (hand2color[0], "gi"),
+        allhair = new RegExp (haircolor[0], "gi"),
+        allneck = new RegExp (neckcolor[0], "gi"),
+        allnose = new RegExp (nosecolor[0], "gi"),
+        allother = new RegExp (othercolor[0], "gi"),
+        newfile2 = newfile.replace(allskin, skincolor[i]),
+        newfile2 = newfile2.replace(allhand, handcolor[i]),
+        newfile2 = newfile2.replace(allhand2, hand2color[i]),
+        newfile2 = newfile2.replace(allhair, haircolor[i]),
+        newfile2 = newfile2.replace(allneck, neckcolor[i]),
+        newfile2 = newfile2.replace(allnose, nosecolor[i]),
+        newfile2 = newfile2.replace(allother, othercolor[i]);
     fs.writeFileSync(`svgs/${newname}`, newfile2);
   }
+});
+
+console.log("Converting Z-marked emojis");
+
+var wfiles = fs.readdirSync("svgs").filter(fn => fn.match(/([w])/i));
+wfiles.forEach(function (item, index) {
+  for (var i = 1; i < 7; i++) {
+    let newnamew = item.replace(/w/g, skincolorvaluesw[i]);
+    fs.copyFileSync(`svgs/${item}`, `svgs/${newnamew}`);
+    var newfilew = fs.readFileSync(`svgs/${newnamew}`, "utf8");
+        allskinw = new RegExp (skincolorw[0], "gi"),
+        allhandw = new RegExp (handcolorw[0], "gi"),
+        allhand2w = new RegExp (hand2colorw[0], "gi"),
+        allhairw = new RegExp (haircolorw[0], "gi"),
+        allneckw = new RegExp (neckcolorw[0], "gi"),
+        allnosew = new RegExp (nosecolorw[0], "gi"),
+        allotherw = new RegExp (othercolorw[0], "gi"),
+        newfile2w = newfilew.replace(allskinw, skincolorw[i]),
+        newfile2w = newfile2w.replace(allhandw, handcolorw[i]),
+        newfile2w = newfile2w.replace(allhand2w, hand2colorw[i]),
+        newfile2w = newfile2w.replace(allhairw, haircolorw[i]),
+        newfile2w = newfile2w.replace(allneckw, neckcolorw[i]),
+        newfile2w = newfile2w.replace(allnosew, nosecolorw[i]),
+        newfile2w = newfile2w.replace(allotherw, othercolorw[i]);
+    fs.writeFileSync(`svgs/${newnamew}`, newfile2w);
+  }
+});
+
+console.log("Deleting Z-marked emojis");
+
+var wfiles = fs.readdirSync("svgs").filter(fn => fn.match(/([w])/i));
+wfiles.forEach(function (item, index) {
+  fs.unlinkSync(`svgs/${item}`)
 });
 
 console.log("Copying regular emojis");
