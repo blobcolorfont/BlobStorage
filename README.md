ab41f3b7b72d85124cda1b74c4700c9e6967179b
joaoh1 <joaoh1@users.noreply.github.com>
2019-09-28 23:33:42 -0300

    Build Script Rework Extravaganza It has been barely 10 commits but another extravaganza was made. This extravaganza targets the build.js script, heavily reworking it. /svgs/ and /pngs/ has been changed to /svg/ & /png/, RIP BlobCompiler. Build steps have been merged and optimized. var has been changed to either let or const. /src/all/ is now gone since now it's automatically merged inside /svg/. ImageMagick is no longer needed at all! It has been replaced by Sharp. A package.json has been created, now with a script to run build. The emoji from the past commit have been built, vomiting face changed. LICENSE has been updated to be more accurate to Google's part.
    Even after all the changes, it's unknown if the issue that inflates...
    ...the change count has been fixed.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # BlobStorage
-This is the repository where the unpolished SVGs for the Blob Color Font project are stored.<br>
+This is the repository where the source SVGs, the optimized SVGs and 128x PNGs for the Blob Color Font project are stored.<br>
 The emoji are either based on or directly from the [Noto Color Emoji](https://github.com/googlei18n/noto-emoji/) before the Oreo update.<br>
-Using SVGO is recommended before using them for anything.<br>
+All the output files were generated and can be used without having to build anything.<br>
 
 ## Usage
 The BlobStorage is being used in the [BlobCompiler](https://github.com/blobcolorfont/BlobCompiler/) project, where it will build a SVGinOT font with all the emojis here.<br>
 Our emojis can be used outside the font project as long as you credit us.
 
-## Building /svgs/ and /pngs/
-To build the SVGs along with the skin-colored variants, you need [Node.js](https://nodejs.org/en/) and the [svgo](https://www.npmjs.com/package/svgo) package installed and to build the PNGs from the generated SVGs, you need [ImageMagick](https://imagemagick.org/)<br>
-After they are installed, execute ``node build.js`` and the /svgs/ and /pngs/ folder will be built from /src/all/
+## Building /svg/ and /png/
+To build the SVG along with the skin-colored variants, you need [Node.js](https://nodejs.org/en/) installed, the packages (svgo and sharp) are necessary to optimize and export the SVGs, which can be done by executing ``npm install``.<br>
+After they are installed, execute ``npm build`` and the /svg/ and /png/ folder will be built from all the folders from /src/.
 
 ## License
 All the image resources released under this repository are licensed under the [Apache license, version 2.0](./LICENSE).
--- build.js
+++ build.js
@@ -1,78 +1,45 @@
 const fs = require('fs');
-const path = require('path');
+const sharp = require('sharp');
 const svgo = require('svgo');
-const {exec} = require('child_process');
-const optimizer = new svgo({
-        plugins: [{
-          cleanupAttrs: true,
-        }, {
-          removeDoctype: true,
-        },{
-          removeXMLProcInst: true,
-        },{
-          removeComments: true,
-        },{
-          removeMetadata: true,
-        },{
-          removeTitle: true,
-        },{
-          removeDesc: true,
-        },{
-          removeUselessDefs: true,
-        },{
-          removeEditorsNSData: true,
-        },{
-          removeEmptyAttrs: true,
-        },{
-          removeHiddenElems: true,
-        },{
-          removeEmptyText: true,
-        },{
-          removeEmptyContainers: true,
-        },{
-          removeViewBox: false,
-        },{
-          cleanupEnableBackground: true,
-        },{
-          convertStyleToAttrs: true,
-        },{
-          convertColors: true,
-        },{
-          convertPathData: true,
-        },{
-          convertTransform: true,
-        },{
-          removeUnknownsAndDefaults: true,
-        },{
-          removeNonInheritableGroupAttrs: true,
-        },{
-          removeUselessStrokeAndFill: true,
-        },{
-          removeUnusedNS: true,
-        },{
-          cleanupIDs: true,
-        },{
-          cleanupNumericValues: true,
-        },{
-          moveElemsAttrsToGroup: true,
-        },{
-          moveGroupAttrsToElems: true,
-        },{
-          collapseGroups: true,
-        },{
-          removeRasterImages: false,
-        },{
-          mergePaths: true,
-        },{
-          convertShapeToPath: true,
-        },{
-          sortAttrs: true,
-        },{
-          removeDimensions: true,
-        }]
+const svgoInstance = new svgo({
+  plugins: [
+    {cleanupAttrs: true}, 
+    {removeDoctype: true},
+    {removeXMLProcInst: true},
+    {removeComments: true},
+    {removeMetadata: true},
+    {removeTitle: true},
+    {removeDesc: true},
+    {removeUselessDefs: true},
+    {removeEditorsNSData: true},
+    {removeEmptyAttrs: true},
+    {removeHiddenElems: true},
+    {removeEmptyText: true},
+    {removeEmptyContainers: true},
+    {removeViewBox: false},
+    {cleanupEnableBackground: true},
+    {convertStyleToAttrs: true},
+    {convertColors: true},
+    {convertPathData: true},
+    {convertTransform: true},
+    {removeUnknownsAndDefaults: true},
+    {removeNonInheritableGroupAttrs: true},
+    {removeUselessStrokeAndFill: true},
+    {removeUnusedNS: true},
+    {cleanupIDs: true},
+    {cleanupNumericValues: true},
+    {moveElemsAttrsToGroup: true},
+    {moveGroupAttrsToElems: true},
+    {collapseGroups: true},
+    {removeRasterImages: false},
+    {mergePaths: true},
+    {convertShapeToPath: true},
+    {sortAttrs: true},
+    {removeDimensions: true}
+  ]
 });
 
-var skincolorvalues = ["", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"];
+const skincolorvalues = ["", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"],
     skincolor = ["FCC21B", "FADCBC", "E0BB95", "CC9970", "AD6F43", "8E726A"],
     handcolor = ["FAC036", "FADCBC", "E0BB95", "CC9970", "AD6F43", "8E726A"],
     hand2color = ["E39E49", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
@@ -81,23 +48,31 @@ var skincolorvalues = ["", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"];
     othercolor = ["E59600", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
     nosecolor = ["E48C15", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"];
 
-var skincolorvaluesw = ["-test", "", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"];
+const skincolorvaluesw = ["-test", "", "-1F3FB", "-1F3FC", "-1F3FD", "-1F3FE", "-1F3FF"],
     skincolorw = ["FFFF00", "FCC21B", "FADCBC", "E0BB95", "CC9970", "AD6F43", "8E726A"],
     handcolorw = ["FFCC00", "FAC036", "FADCBC", "E0BB95", "CC9970", "AD6F43", "8E726A"],
     hand2colorw = ["00CCFF", "E39E49", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
     haircolorw = ["FF0000", "FFA000", "312D2D", "BFA055", "6D4C41", "47352D", "232020"],
     neckcolorw = ["00FF00", "E59900", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
     othercolorw = ["0000FF", "E59600", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
-    nosecolorw = ["FF00FF", "E48C15", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"],
+    nosecolorw = ["FF00FF", "E48C15", "DBA689", "C48E6A", "99674F", "7A4C32", "563E37"];
+
+const srcfolders = fs.readdirSync("src");
+srcfolders.forEach((name) => {
+    const path = `src/${name}`;
+    const subfolderfiles = fs.readdirSync(path);
+    subfolderfiles.forEach((name) => {
+        fs.copyFileSync(`${path}/${name}`, `svg/${name}`)
+    });
+});
 
 console.log("Converting X-marked emojis");
-
-var xfiles = fs.readdirSync("src/all").filter(fn => fn.match(/([x])/i));
-xfiles.forEach(function (item, index) {
-  for (var i = 0; i < 6; i++) {
+const xfiles = fs.readdirSync("svg").filter(fn => fn.match(/([x])/i));
+xfiles.forEach(function (item) {
+  for (let i = 0; i < 6; i++) {
     let newname = item.replace(/x/g, skincolorvalues[i]);
-    fs.copyFileSync(`src/all/${item}`, `svgs/${newname}`);
-    var newfile = fs.readFileSync(`svgs/${newname}`, "utf8");
+    fs.copyFileSync(`svg/${item}`, `svg/${newname}`);
+    let newfile = fs.readFileSync(`svg/${newname}`, "utf8");
         allskin = new RegExp (skincolor[0], "gi"),
         allhand = new RegExp (handcolor[0], "gi"),
         allhand2 = new RegExp (hand2color[0], "gi"),
@@ -105,25 +80,25 @@ xfiles.forEach(function (item, index) {
         allneck = new RegExp (neckcolor[0], "gi"),
         allnose = new RegExp (nosecolor[0], "gi"),
         allother = new RegExp (othercolor[0], "gi"),
-        newfile2 = newfile.replace(allskin, skincolor[i]),
-        newfile2 = newfile2.replace(allhand, handcolor[i]),
-        newfile2 = newfile2.replace(allhand2, hand2color[i]),
-        newfile2 = newfile2.replace(allhair, haircolor[i]),
-        newfile2 = newfile2.replace(allneck, neckcolor[i]),
-        newfile2 = newfile2.replace(allnose, nosecolor[i]),
-        newfile2 = newfile2.replace(allother, othercolor[i]);
-    fs.writeFileSync(`svgs/${newname}`, newfile2);
-  }
+        newfile = newfile.replace(allskin, skincolor[i]),
+        newfile = newfile.replace(allhand, handcolor[i]),
+        newfile = newfile.replace(allhand2, hand2color[i]),
+        newfile = newfile.replace(allhair, haircolor[i]),
+        newfile = newfile.replace(allneck, neckcolor[i]),
+        newfile = newfile.replace(allnose, nosecolor[i]),
+        newfile = newfile.replace(allother, othercolor[i]);
+    fs.writeFileSync(`svg/${newname}`, newfile);
+  };
+  if (i = 5) { fs.unlinkSync(`svg/${item}`) };
 });
 
 console.log("Converting Z-marked emojis");
-
-var wfiles = fs.readdirSync("svgs").filter(fn => fn.match(/([w])/i));
-wfiles.forEach(function (item, index) {
-  for (var i = 1; i < 7; i++) {
+const wfiles = fs.readdirSync("svg").filter(fn => fn.match(/([w])/i));
+wfiles.forEach(function (item) {
+  for (let i = 1; i < 7; i++) {
     let newnamew = item.replace(/w/g, skincolorvaluesw[i]);
-    fs.copyFileSync(`svgs/${item}`, `svgs/${newnamew}`);
-    var newfilew = fs.readFileSync(`svgs/${newnamew}`, "utf8");
+    fs.copyFileSync(`svg/${item}`, `svg/${newnamew}`);
+    let newfilew = fs.readFileSync(`svg/${newnamew}`, "utf8");
         allskinw = new RegExp (skincolorw[0], "gi"),
         allhandw = new RegExp (handcolorw[0], "gi"),
         allhand2w = new RegExp (hand2colorw[0], "gi"),
@@ -131,42 +106,31 @@ wfiles.forEach(function (item, index) {
         allneckw = new RegExp (neckcolorw[0], "gi"),
         allnosew = new RegExp (nosecolorw[0], "gi"),
         allotherw = new RegExp (othercolorw[0], "gi"),
-        newfile2w = newfilew.replace(allskinw, skincolorw[i]),
-        newfile2w = newfile2w.replace(allhandw, handcolorw[i]),
-        newfile2w = newfile2w.replace(allhand2w, hand2colorw[i]),
-        newfile2w = newfile2w.replace(allhairw, haircolorw[i]),
-        newfile2w = newfile2w.replace(allneckw, neckcolorw[i]),
-        newfile2w = newfile2w.replace(allnosew, nosecolorw[i]),
-        newfile2w = newfile2w.replace(allotherw, othercolorw[i]);
-    fs.writeFileSync(`svgs/${newnamew}`, newfile2w);
-  }
+        newfilew = newfilew.replace(allskinw, skincolorw[i]),
+        newfilew.replace(allhandw, handcolorw[i]),
+        newfilew = newfilew.replace(allhand2w, hand2colorw[i]),
+        newfilew = newfilew.replace(allhairw, haircolorw[i]),
+        newfilew = newfilew.replace(allneckw, neckcolorw[i]),
+        newfilew = newfilew.replace(allnosew, nosecolorw[i]),
+        newfilew = newfilew.replace(allotherw, othercolorw[i]);
+    fs.writeFileSync(`svg/${newnamew}`, newfilew);
+  };
+  if (i = 6) { fs.unlinkSync(`svg/${item}`) };
 });
 
-console.log("Deleting Z-marked emojis");
+console.log("Optimizing SVGs and exporting to 128x PNGs");
+const ofiles = fs.readdirSync("svg");
+ofiles.forEach(function (item) {
+  const data = fs.readFileSync(`svg/${item}`);
+  svgoInstance.optimize(data, {path: `svg/${item}`}).then(function(result) {
+    fs.writeFileSync(`svg/${item}`, result.data);
 
-var wfiles = fs.readdirSync("svgs").filter(fn => fn.match(/([w])/i));
-wfiles.forEach(function (item, index) {
-  fs.unlinkSync(`svgs/${item}`)
+    sharp(Buffer.from(result.data, 'utf8'), {
+      density: 128
+    })
+    .png()
+    .toFile(`./png/128/${item.replace('svg', 'png')}`)
+  })
 });
-
-console.log("Copying regular emojis");
-
-var files = fs.readdirSync("src/all").filter(fn => fn.match(/^((?!x).)*$/i));
-files.forEach(function (item, index) {
-  fs.copyFileSync(`src/all/${item}`, `svgs/${item}`);
-});
-
-var ofiles = fs.readdirSync("svgs");
-ofiles.forEach(function (item, index) {
-  var optifile = fs.readFile(`svgs/${item}`, "utf8", (err, data) => {
-    optimizer.optimize(data, {path: `svgs/${item}`}).then(function(result) {
-      fs.writeFileSync(`svgs/${item}`, result.data);
-    });
-  });
-});
-
-console.log("Converting SVGs to PNGs");
-
-exec('magick convert -size 128x128 -background "rgba(255,255,255,0)" -set filename:f "%t" svg:./svgs/*.svg png:./pngs/128/"%[filename:f].png"');
 
 console.log("Finishing the build...");
