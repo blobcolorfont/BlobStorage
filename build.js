536841161557c9e466b259365fde80b84c93d6d5
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2019-10-19 23:58:43 -0300

    Clear up all the redundant SVGO plugin settings From the cleanup, the settings got reduced to only 3 plugins. No more wall of plugin toggles.

--- build.js
+++ build.js
@@ -3,40 +3,9 @@ const sharp = require('sharp');
 const svgo = require('svgo');
 const svgoInstance = new svgo({
   plugins: [
-    {cleanupAttrs: true}, 
-    {removeDoctype: true},
-    {removeXMLProcInst: true},
-    {removeComments: true},
-    {removeMetadata: true},
-    {removeTitle: true},
-    {removeDesc: true},
-    {removeUselessDefs: true},
-    {removeEditorsNSData: true},
-    {removeEmptyAttrs: true},
-    {removeHiddenElems: true},
-    {removeEmptyText: true},
-    {removeEmptyContainers: true},
-    {removeViewBox: false},
-    {cleanupEnableBackground: true},
-    {convertStyleToAttrs: true},
-    {convertColors: true},
-    {convertPathData: true},
-    {convertTransform: true},
-    {removeUnknownsAndDefaults: true},
-    {removeNonInheritableGroupAttrs: true},
-    {removeUselessStrokeAndFill: true},
-    {removeUnusedNS: true},
-    {cleanupIDs: true},
-    {cleanupNumericValues: true},
-    {moveElemsAttrsToGroup: true},
-    {moveGroupAttrsToElems: true},
-    {collapseGroups: true},
-    {removeRasterImages: false},
-    {mergePaths: true},
-    {convertShapeToPath: true},
-    {sortAttrs: true},
-    {sortDefsChildren: true},
-    {removeDimensions: true}
+     {removeViewBox: false},
+     {removeRasterImages: true},
+     {removeDimensions: true}
   ]
 });
 
